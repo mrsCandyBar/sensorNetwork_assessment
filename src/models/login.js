8f718a200bcf186edcac616d426df4a36ddd7693
Candice Bär <candicekbar@gmail.com>
2023-05-10 15:54:30 +0200

    fix: remove unused models

--- src/models/login.js
+++
@@ -1,6 +0,0 @@
-export class LoginModel {
-    constructor(obj) {
-        this.email = obj && obj.email ? obj.email : undefined;
-        this.password = obj && obj.password ? obj.password : undefined;
-    }
-}
--- src/models/team.js
+++
@@ -1,8 +0,0 @@
-export class TeamModel {
-    constructor(obj) {
-        this.name = obj && obj.name ? obj.name : undefined;
-        this.phoneNumber = obj && obj.phoneNumber ? obj.phoneNumber : undefined;
-        this.email = obj && obj.email ? obj.email : undefined;
-        this.password = obj && obj.password ? obj.password : undefined;
-    }
-}
